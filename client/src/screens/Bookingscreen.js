7e91779eb8e5ddeb712bb6193f21a13d6185544e
DavideC1999 <civicchionidavide@libero.it>
2023-07-08 10:44:49 +0200

    12(13). Next: 14

--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -3,9 +3,9 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 function Bookingscreen() {
-  const [loading, setLoading] = useState();
+  const [room, setRoom] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
-  const [room, setRoom] = useState();
 
   let {roomid} = useParams();
 
@@ -14,7 +14,6 @@ function Bookingscreen() {
         try {
             setLoading(true);
             const data = (await axios.post("/api/rooms/getroombyid", {roomid : roomid})).data;
-    
             setRoom(data);
             setLoading(false);
           } catch (error) {
@@ -22,11 +21,52 @@ function Bookingscreen() {
             setError(true);
           }
     } 
-    fetchdata()}, []);
-
+    fetchdata()
+  });
   return (
     <div>
-        <h1>{roomid}</h1>
+      {room ? (
+        <div className="m-5">
+
+        <div className="row justify-content-center mt-5 bs">
+
+          <div className="col-md-6">
+            <h1>{room.nome}</h1>
+            <img src={room.urlimmagini[0]} className="bigimg" />
+          </div>
+
+          <div className="col-md-6">
+            <div style={{textAlign: 'right'}}>
+              <h1>Dettagli prenotazione</h1>
+              <hr/>
+              <b>
+              <p>Nome: </p>
+              <p>Da: </p>
+              <p>A: </p>
+              <p>Capienza: {room.capienza}</p>
+              </b>
+            </div>
+            <div style={{textAlign: 'right'}}>
+              <h1>Resoconto</h1>
+              <hr/>
+              <b>
+              <p>Giorni totali: 2 </p>
+              <p>Affitto giornaliero: {room.costo}</p>
+              <p>Totale: {}</p>
+              </b>
+            </div>
+            <div style={{float: 'right'}}>
+              <button className="btn btn-primary">Paga ora</button>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+        ) 
+        : loading ? (<h1>Loading....</h1>)
+        : (<h1>Error: {error}</h1>)
+      }
     </div>
   );
 }
